407e10954ec50dad6a3521705000959bcd8d5299
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-12-01 17:32:13 +0200

    fix tasks.py

--- tasks.py
+++ tasks.py
@@ -7,10 +7,12 @@ def start(ctx):
     ctx.run("python3 backend/src/main.py", pty=True)
 
 
+@task
 def test(ctx):
     ctx.run('pytest backend/src', pty=True)
 
 
+@task
 def coverage(ctx):
     ctx.run('coverage run --branch -m pytest backend/src', pty=True)
 
